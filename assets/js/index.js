0c8f12af64a81a9d12b495fea01a3d09c1a20683
Riccardo Limiti <riccardo.limiti@gmail.com>
2021-08-25 16:37:28 +0200

    solution added

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,3 +27,57 @@ const books = [
     img: "https://eloquentjavascript.net/img/cover.jpg"
   }
 ];
+
+const bigBook = document.querySelector('.book-list')
+
+books.forEach(element=> {
+const listItem = document.createElement('li')
+listItem.classList.add('book', 'mb-3', 'card')
+const bookDiv = document.createElement('div')
+bookDiv.classList.add('card-body')
+const bookH2 = document.createElement('h2')
+bookH2.classList.add('book-title', 'card-title', 'h6')
+bookH2.textContent = element.author.split(` `).reverse().join(` `)
+const bookP = document.createElement('p')
+bookP.textContent = element.title
+bookP.classList.add('book-author', 'card-subtitle', 'text-muted', 'small')
+const bookFooter = document.createElement('footer')
+const bookSpan = document.createElement('span')
+const bookImg = document.createElement('img')
+bookImg.classList.add(`book-cover`, `card-img-top`, `img-fluid`)
+bookImg.src = element.img
+const bookLinks = document.createElement('a')
+bookLinks.href = element.img
+bookSpan.classList = `badge rounded-pill`
+bookSpan.style.backgroundColor = `lightgreen`
+bookFooter.appendChild(bookSpan)
+bookLinks.appendChild(bookImg)
+bookDiv.appendChild(bookP)
+bookDiv.appendChild(bookH2)
+listItem.appendChild(bookLinks)
+listItem.appendChild(bookDiv)
+listItem.appendChild(bookFooter)
+listItem.appendChild(bookImg) 
+bookLinks.appendChild(bookImg)
+bigBook.appendChild(listItem)
+bookFooter.style.display = `flex`
+bookFooter.style.justifyContent = `center`
+bookFooter.style.padding = `20px`
+
+function readToRead() {
+if (element.alreadyRead == true) {
+return (bookSpan.innerText = 'Read');
+} else {
+return (
+(bookSpan.innerText = 'To be Read'), (bookSpan.style.backgroundColor = 'red')
+)
+}
+}
+readToRead(); 
+});
+
+
+
+
+
+
